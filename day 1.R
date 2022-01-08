2a4934d0ce3c0ad8fccd812f341fc5df0660ca4f
maxull <max.ull@online.no>
2022-01-08 13:04:20 +0100

    data filtering

--- day 1.R
+++ day 1.R
@@ -1,4 +1,4 @@
-library(readxl); library(tidyverse)
+library(readxl); library(tidyverse); library(dataPreparation)
 
 
 
@@ -20,7 +20,7 @@ summary(Fitness_Females)
 summary(Fitness_Males)
 
 ##################################################################
-#### normality checks ###########################################
+#### normality checks female #####################################
 
 
 Fitness_Females %>% 
@@ -35,15 +35,58 @@ Fitness_Females %>%
         ggplot(aes(Est_VO2_max))+
         geom_histogram()
 
-############ here
+shapiro.test(Fitness_Females$Stature)
+
+shapiro.test(Fitness_Females$Body_Mass)
+
+shapiro.test(Fitness_Females$Est_VO2_max)
 
 
+Fitness_Males %>% 
+        ggplot(aes(Stature))+
+        geom_histogram()
 
+Fitness_Males %>% 
+        ggplot(aes(Body_Mass))+
+        geom_histogram()
+
+Fitness_Males %>% 
+        ggplot(aes(Est_VO2_max))+
+        geom_histogram()
+
+shapiro.test(Fitness_Males$Stature)
+
+shapiro.test(Fitness_Males$Body_Mass)
+
+shapiro.test(Fitness_Males$Est_VO2_max)
 
 
 Fitness_Females %>% 
-        ggplot(aes(Stature))+
+        ggplot(aes(sample = Stature))+
+        geom_qq()+
+        geom_qq_line()
+
+
+########################################################################################################
+###### filter outliers >3sd      #######################################################################
+
+Fitness_Females2 <- remove_sd_outlier(Fitness_Females,cols = "Body_Mass" ,n_sigmas = 3)
+
+Fitness_Females2 %>% 
+        ggplot(aes(Est_VO2_max))+
         geom_histogram()
+
+Fitness_Males2 <- remove_sd_outlier(Fitness_Males,cols = "Body_Mass" ,n_sigmas = 3)
+
+Fitness_Males2 %>% 
+        ggplot(aes(Est_VO2_max))+
+        geom_histogram()
+
+
+
+
+
+
 
 summary(Fitness_Females) %>% 
         print()
