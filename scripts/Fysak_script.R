e32dab26edc6650848205235805f266c44ed8def
maxull <max.ull@online.no>
2022-01-12 12:44:54 +0100

    safety save

--- scripts/Fysak_script.R
+++ scripts/Fysak_script.R
@@ -3,7 +3,8 @@
 library(foreign); library(tidyverse)
 
 ### read SPSS file, .sav filetype
-
+### library(foreign), must use argument "to.data.frame = TRUE" otherwise you get the data variables page
 
 
 Fysak <- read.spss("data/Datasett 13 FYSAK (2).sav",to.data.frame = TRUE)
+
