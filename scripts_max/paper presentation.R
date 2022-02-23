87ab0dacf9bffc4263082b45fbcd36a3e7b488ee
maxull <max.ull@online.no>
2022-02-23 12:37:04 +0100

    paper

---
+++ scripts_max/paper presentation.R
@@ -0,0 +1,17 @@
+library(foreign); library(tidyverse); library(psych);library(readxl);library(gtsummary); library(gmodels)
+
+tabel2 <- read_excel("data/Book1.xlsx, col_names = TRUE) %>% 
+        print()
+
+
+
+tabel2 %>% 
+        tbl_summary(by = Group)
+
+#data13 %>% 
+        select(ASTMA,Stillesitting) %>% 
+        tbl_summary(by = ASTMA,
+                    statistic = c(Stillesitting) ~"{mean} ({sd})", 
+                    digits = ~ 2) %>% 
+        add_p(Stillesitting~"t.test", test.args = Stillesitting~list(var.equal = TRUE)) %>% 
+        add_ci()
