acde1cb289557f772af10b1c46814c3d914507d4
maxull <max.ull@online.no>
2022-03-29 22:26:38 +0200

    methylation

---
+++ scripts_max/Methylation script.R
@@ -0,0 +1,10 @@
+
+
+#######################################
+#### methylation data analysis ########
+#######################################
+
+if (!require("BiocManager", quietly = TRUE))
+        install.packages("BiocManager")
+
+BiocManager::install("minfi")
