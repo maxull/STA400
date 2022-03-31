805703e6b7407f00d0d6fa8c2d2d8a6cb9f7c28d
maxull <max.ull@online.no>
2022-03-31 23:02:04 +0200

    ChAMP test run success

--- scripts_max/Methylation script.R
+++ scripts_max/Methylation script.R
@@ -4,6 +4,8 @@
 #### methylation data analysis ########
 #######################################
 
+library(tidyr)
+
 if (!require("BiocManager", quietly = TRUE))
         install.packages("BiocManager")
 
@@ -33,5 +35,48 @@ warnings()
 
 ### seemes like ChAMP is installed
 ### test on included dataset to se if it works
+### pipeline from:
+### https://bioconductor.org/packages/devel/bioc/vignettes/ChAMP/inst/doc/ChAMP.html
 
 
+testDir=system.file("extdata", package = "ChAMPdata") %>% 
+        myLoad<- champ.load(testDir, arraytype = "450k")
+
+myLoad <- champ.load(testDir,arraytype = "450k")
+
+### show warnings
+
+warnings()
+
+### dont understand, but maybe not an issue
+
+### next step in pipeline: champ.qc
+### quality control and initial visualization
+
+champ.QC()
+
+CpG.GUI()
+
+QC.GUI()
+
+### normalize data, chose nethod "SWAN" or functional normalization
+
+myNorm <- champ.norm()                  #### find out how to chose functional
+
+champ.SVD()                             #### not sure what this does "singular value decomposition"
+
+### identify differentially methylated positions
+
+myDMP <- champ.DMP()
+
+### visualize
+
+DMP.GUI()
+
+### identify DMR
+
+myDMR <- champ.DMR()
+
+### visualize DMR
+
+DMR.GUI()
