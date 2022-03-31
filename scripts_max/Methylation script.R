9cbd6b123e67a9057a2574c85808f92859e72488
maxull <max.ull@online.no>
2022-03-31 20:47:16 +0200

    ChAMP

--- scripts_max/Methylation script.R
+++ scripts_max/Methylation script.R
@@ -8,3 +8,28 @@ if (!require("BiocManager", quietly = TRUE))
         install.packages("BiocManager")
 
 BiocManager::install("minfi")
+
+if (!require("BiocManager", quietly = TRUE))
+        install.packages("BiocManager")
+
+BiocManager::install("ChAMP", force = TRUE)
+
+library("ChAMP")
+
+if (!requireNamespace("BiocManager", quietly=TRUE))
+        install.packages("BiocManager")
+BiocManager::install(c("minfi","ChAMPdata","Illumina450ProbeVariants.db",
+                       "sva","IlluminaHumanMethylation450kmanifest","limma",
+                       "RPMM","DNAcopy","preprocessCore","impute","marray",
+                       "wateRmelon","goseq","plyr","GenomicRanges","RefFreeEWAS",
+                       "qvalue","isva","doParallel","bumphunter","quadprog","shiny",
+                       "shinythemes","plotly","RColorBrewer","DMRcate","dendextend",
+                       "IlluminaHumanMethylationEPICmanifest","FEM","matrixStats","missMethyl","combinat", force = "TRUE"))
+
+BiocManager::install("YourErrorPackage")
+
+myLoad <- ChAMP::Champ.load("data/GSM4948519.txt", arraytype = "EPIC")
+
+BiocManager::install("locfit")
+
+BiocManager::install("DMRcate")
