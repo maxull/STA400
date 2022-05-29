31ca3913294d3a2e6097ef7cc086fe897976cfa7
maxull <max.ull@online.no>
2022-05-29 22:04:56 +0200

    loading methylation data from repeated hypertrophy

--- scripts_max/repeated hypertrophy/m_analysis.R
+++ scripts_max/repeated hypertrophy/m_analysis.R
@@ -4,14 +4,30 @@
 ###
 ################################################################
 
-library(wateRmelon); library(methylumi);library(FDb.InfiniumMethylation.hg19);library(minfi); library(maxprobes); library(tidyverse);library(ggplot2); library(GEOquery)
+library(wateRmelon); library(methylumi);library(FDb.InfiniumMethylation.hg19);library(minfi); library(maxprobes); library(tidyverse);library(ggplot2); library(GEOquery); library(plyr)
 
 
 path <- "C:/Users/maxul/OneDrive/Dokumenter/Skole/Master 21-22/Master/memory_of_hypertrophy_data/GSE114763_RAW.tar"
 
 untar(path, exdir = "C:/Users/maxul/OneDrive/Dokumenter/Skole/Master 21-22/Master/memory_of_hypertrophy_data/untared")
 
-testdir = ("C:/Users/maxul/OneDrive/Dokumenter/Skole/Master 21-22/Master/memory_of_hypertrophy_data/untared")
-targets <- read.metharray.sheet("C:/Users/maxul/OneDrive/Dokumenter/Skole/Master 21-22/Master/memory_of_hypertrophy_data/untared/GPL21145_MethylationEPIC_15073387_v-1-0.csv.gz")
 
-list.files(testdir)
+
+my_dir <- "/Users/maxul/OneDrive/Dokumenter/Skole/Master 21-22/Master/memory_of_hypertrophy_data/untared"
+
+zip_file <- list.files(path = my_dir, pattern = "*.gz", full.names = TRUE)
+
+ldply(.data = zip_file, .fun = gunzip)
+
+targets <- read.metharray.sheet(my_dir)
+
+rgSet <- read.metharray.exp(targets = targets)
+
+targets$ID <- paste(targets$Sample_Group,targets$Sample_Name,sep=".")
+sampleNames(rgSet) <- targets$ID
+
+
+
+myLoad2 <- champ.load(my_dir, arraytype = "EPIC", method = "minfi")
+
+
